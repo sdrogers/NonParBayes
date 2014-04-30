93e63ecdfd16bf8c4abc8a2a2a6ae81851258cfb
sdrogers <simon.d.rogers@gmail.com>
2014-04-30 12:46:07 +0100

    finished GP bits (except tasks)

--- Code/GP/GPintro.m
+++ Code/GP/GPintro.m
@@ -182,7 +182,7 @@ feval(plotfunction,'gpintro_withnoise.eps');
 
 %% Classification example
 % Generate some data
-gamvals = [1 5];
+gamvals = [1 5 10];
 close all;
 x = [randn(20,2)+repmat([2 0],20,1);
     randn(20,2) + repmat([-2 0],20,1);
@@ -258,4 +258,255 @@ for g = 1:length(gamvals)
     setupPlot;
     fname = sprintf('gpclass_hyp%g_surf.eps',gam);
     feval(@makePDFopengl,fname);
+end
+
+%% Generative process
+close all
+N = 200;
+x = randn(N,2);
+gam = 1;
+C = kernel(x,x,'gauss',gam) + 1e-6*eye(N);
+f = gausssamp(zeros(N,1),C,1)';
+z = randn(N,1) + f;
+y = zeros(N,1);
+y(z<0) = 0;
+y(z>0) = 1;
+plotClassData(x,y);
+
+%% Multi-class example
+x = [randn(20,2)+repmat([2 0],20,1);
+    randn(20,2) + repmat([-2 0],20,1);
+    randn(30,2) + repmat([0 2],30,1)];
+
+y = [repmat(1,20,1);repmat(2,20,1);repmat(3,30,1)];
+
+
+[X,Y] = meshgrid(xl(1):0.2:xl(2),yl(1):0.2:yl(2));
+testx = [reshape(X,prod(size(X)),1) reshape(Y,prod(size(Y)),1)];
+
+gam = 1;
+testC = kernel(testx,x,'gauss',gam);
+
+% Do some sampling
+C = kernel(x,x,'gauss',gam);
+Ci = inv(C);
+N = size(x,1);
+
+K = max(y);
+for k = 1:K
+    f(:,k) = gausssamp(repmat(0,N,1),C,1)';
+end
+
+z = zeros(size(f));
+NITS = 5000;
+BURN = 1000;
+postC = inv(Ci + eye(N));
+testprobs = zeros(size(testx,1),K);
+testvar = zeros(size(testx,1),1);
+Cprod = diag(testC*(C\testC'));
+testvar = repmat(1,size(testx,1),1) - Cprod;
+
+for it = 1:NITS
+    fprintf('Iteration %g\n',it);
+    for n = 1:N
+        % sample z_n
+        z(n,:) = randn(1,K) + f(n,:);
+        pos = find(z(n,:)==max(z(n,:)),1);
+        while pos ~= y(n)
+            z(n,:) = randn(1,K) + f(n,:);
+            pos = find(z(n,:)==max(z(n,:)),1);
+        end
+    end
+    % Sample f
+    for k = 1:K
+        f(:,k) = gausssamp(postC*z(:,k),postC,1)';
+    end
+    
+    % Do the regression for predictions
+    
+    testmu = (testC/C)*f;
+    
+
+    testsample = randn(size(testx,1),K).*repmat(sqrt(testvar),1,K) + testmu;
+    testz = testsample + randn(size(testx,1),K);
+    if it > BURN
+        for n = 1:size(testx,1)
+            pos = find(testz(n,:) == max(testz(n,:)),1);
+            testprobs(n,pos) = testprobs(n,pos) + 1;
+        end
+    end
+end
+
+testprobs = testprobs ./ repmat(sum(testprobs,2),1,K);
+
+for k = 1:K
+    close all
+    surf(X,Y,reshape(testprobs(:,k),size(X)));
+    view([0 0 1]);
+    hold on
+    plotClassData3(x,y);
+    setupPlot;
+    fname = sprintf('multiclass_k%g.eps',k);
+    feval(@makePDFopengl,fname);
+end
+
+
+close all
+plotClassData3(x,y);
+setupPlot;
+fname = 'multiclass_data.eps';
+feval(@makePDF,fname);
+
+
+%% Visualising aux variable trick
+close all
+f = 0.7;
+xvals1 = [-3:0.01:0];
+xvals2 = [0:0.01:5];
+p1x = [xvals1 xvals1(end) xvals1(1)];
+p1y = [normpdf(xvals1,f,1),0,0];
+
+p2x = [xvals2 xvals2(end) xvals2(1)];
+p2y = [normpdf(xvals2,f,1),0,0];
+patch(p1x,p1y,[0.8 0.8 0.8]);
+patch(p2x,p2y,[0.6 0.6 0.6]);
+axis tight
+hold on
+plot([0 0],ylim,'k--');
+% text(f+0.1,0.1,'$f_n$','interpreter','latex','fontsize',24)
+text(0.5,0.2,'$y_n=1$','interpreter','latex','fontsize',24)
+text(-2.5,0.2,'$y_n=-1$','interpreter','latex','fontsize',24)
+setupPlot
+xlabel('$z_n$','interpreter','latex','fontsize',24)
+makePDF('vis_aux.eps');
+
+%% And with the ncnm
+close all
+f = 0.7;
+xvals1 = [-3:0.01:-0.5];
+xvals2 = [0.5:0.01:5];
+p1x = [xvals1 xvals1(end) xvals1(1)];
+p1y = [normpdf(xvals1,f,1),0,0];
+
+p2x = [xvals2 xvals2(end) xvals2(1)];
+p2y = [normpdf(xvals2,f,1),0,0];
+patch(p1x,p1y,[0.8 0.8 0.8]);
+patch(p2x,p2y,[0.6 0.6 0.6]);
+axis tight
+hold on
+plot([-0.5 -0.5],ylim,'k--');
+plot([0.5 0.5],ylim,'k--');
+% text(f+0.1,0.1,'$f_n$','interpreter','latex','fontsize',24)
+text(1.0,0.2,'$y_n=1$','interpreter','latex','fontsize',24)
+text(-2.7,0.2,'$y_n=-1$','interpreter','latex','fontsize',24)
+h = text(0,0.15,'$y_n=0$','interpreter','latex','fontsize',24,'rotation',90)
+setupPlot
+xlabel('$z_n$','interpreter','latex','fontsize',24)
+makePDF('vis_aux_ncnm.eps');
+
+
+%% NCNM example
+close all
+data = twospirals(400,100,5,100.0);
+y = data(:,3);x = data(:,1:2);
+y(y==0) = -1;
+N = size(y,1);
+g = zeros(N,1);
+
+% pos = find(y==-1);
+% x(pos,2) = x(pos,2) - 1;
+
+plotClassData3SS(x,y,g)
+
+
+
+
+g(x(:,2)>1) = 1;
+g(x(:,2)<-1) = 1;
+
+
+
+xl = xlim;
+yl = ylim;
+
+[X,Y] = meshgrid(xl(1):0.1:xl(2),yl(1):0.1:yl(2));
+testx = [reshape(X,prod(size(X)),1) reshape(Y,prod(size(Y)),1)];
+
+gam = 1;
+testC = kernel(testx,x,'gauss',gam);
+
+% Do some sampling
+N = size(x,1);
+C = kernel(x,x,'gauss',gam) + 1e-6*eye(N);
+Ci = inv(C);
+
+postC = inv(Ci + eye(N));
+
+
+for a = [0 0.5]
+
+    close all
+    
+    testprobs = zeros(size(testx,1),3);
+    testvar = zeros(size(testx,1),1);
+    Cprod = diag(testC*(C\testC'));
+    testvar = repmat(1,size(testx,1),1) - Cprod;
+
+
+    f = zeros(N,1);
+    f = gausssamp(repmat(0,N,1),C,1)';
+    z = zeros(N,1);
+    NITS = 200;
+    BURN = 100;
+    tempy = y;
+    for it = 1:NITS
+        fprintf('Iteration: %g\n',it);
+        for n = 1:N
+            if g(n) == 1
+                probm = normcdf(-a-f(n));
+                probm = probm / ...
+                    (probm + normcdf(f(n)-a));
+                if rand<probm
+                    % Sample from the negative truncation
+                    tempy(n) = -1;
+                else
+                    tempy(n) = 1;
+                end
+            end
+            z(n) = f(n) + randn;
+            while z(n)*tempy(n)<a
+                z(n) = f(n) + randn;
+            end
+        end
+        f = gausssamp(postC*z,postC,1)';
+
+        % Do the regression for predictions
+        testmu = (testC/C)*f;
+
+        testsample = randn(size(testx,1),1).*sqrt(testvar) + testmu;
+        testz = testsample + randn(size(testx,1),1);
+        if it>BURN
+            pos = find(testz < -a);
+            testprobs(pos,1) = testprobs(pos,1) + 1;
+            pos = find(testz > a);
+            testprobs(pos,3) = testprobs(pos,3) + 1;        
+            pos = find(testz > -a & testz < a);
+            testprobs(pos,2) = testprobs(pos,2) + 1;        
+
+        end
+
+
+
+    end
+
+    testprobs = testprobs./repmat(sum(testprobs,2),1,3);
+    close all
+    surf(X,Y,reshape(testprobs(:,1),size(X)));
+    view([0 0 1]);
+    hold on
+    plotClassData3SS(x,y,g);
+    axis tight
+    setupPlot;
+    fname = sprintf('ncnm_a%g_g%g.eps',10*a,gam);
+    makePDFopengl(fname);
 end
--- Code/GP/plotClassData3SS.m
+++ Code/GP/plotClassData3SS.m
@@ -1,9 +1,12 @@
-function plotClassData3(x,t)
+function plotClassData3SS(x,t,g)
 u = unique(t);
 hold on
 symbs = {'ko','ks','kv'};
 cols = {[1 1 1],[0.6 0.6 0.6],[0.3 0.3 0.3]};
 for i = 1:length(u)
-    pos = find(t==u(i));
+    pos = find(t==u(i) & g==0);
     plot3(x(pos,1),x(pos,2),repmat(1,length(pos),1),symbs{i},'markerfacecolor',cols{i},'markersize',20);
 end
+
+pos = find(g==1)
+plot3(x(pos,1),x(pos,2),repmat(1,length(pos),1),'ko','markersize',5,'markerfacecolor','k');
