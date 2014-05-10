6d40beba53524a72ed5869daeba35d7d670bb130
sdrogers <simon.d.rogers@gmail.com>
2014-05-10 17:37:13 +0100

    added hdp task

---
+++ Code/Tasks/HDP_task.m
@@ -0,0 +1,90 @@
+%% Generate some data from a HDP
+clear all;close all;
+base_mean = 0;
+base_prec = 1e-2;
+
+obs_prec = 1;
+
+J = 5; % Number of files
+N = 50; % 100 observations in all files
+top_components = []; % Store the top components
+top_counts = [];
+nTop = 0;
+
+alpha = 1; %Table DP
+gamma = 1; % Component DP
+
+for j = 1:J
+    Z{j} = [];
+    x{j} = [];
+    cluster_counts{j} = [];
+    tables_to_top{j} = [];
+    nClusters{j} = 0;
+    for n = 1:N
+        % Choose a table
+        probs = [cluster_counts{j} alpha];
+        probs = cumsum(probs./sum(probs));
+        table_choice = find(rand<=probs,1);
+        if table_choice > nClusters{j}
+            % Have to make a new table
+            nClusters{j} = nClusters{j} + 1;
+            Z{j}(n,1) = nClusters{j};
+            cluster_counts{j}(nClusters{j}) = 1;
+            
+            % Choose a top level component for this new table
+            probs = [top_counts gamma];
+            probs = cumsum(probs./sum(probs));
+            top_choice = find(rand<=probs,1);
+            
+            if top_choice > nTop
+                % Create a new top level
+                nTop = nTop + 1;
+                top_counts(1,nTop) = 1;
+                top_components(1,nTop) = randn./sqrt(base_prec) + base_mean;
+                tables_to_top{j}(nClusters{j}) = nTop;
+                x{j}(n,1) = randn./sqrt(obs_prec) + top_components(tables_to_top{j}(nClusters{j}));
+            else
+                % Use the current one
+               tables_to_top{j}(nClusters{j}) = top_choice;
+               top_counts(top_choice) = top_counts(top_choice) + 1;
+               x{j}(n,1) = randn./sqrt(obs_prec) + top_components(tables_to_top{j}(nClusters{j}));
+            end
+           
+        else
+            % Add to the current table
+            Z{j}(n,1) = table_choice;
+            cluster_counts{j}(table_choice) = cluster_counts{j}(table_choice) + 1;
+            x{j}(n,1) = randn./sqrt(obs_prec) + top_components(tables_to_top{j}(table_choice));
+        end
+    end
+            
+end
+
+
+
+% Plot HDP data
+close all
+symbs = {'ro','k^','bs','g*','mv'};
+for j = 1:J
+    plot(x{j},j,symbs{j},'markersize',10);
+    hold on
+end
+
+yl = ylim;
+for t = 1:length(top_components)
+    plot([top_components(t) top_components(t)],[yl],'k--','linewidth',2);
+end
+
+set(gca,'ytick',[1:J]);
+xlabel('x');
+ylabel('Dataset');
+
+%%
+out = hdp_sample(x)
+
+close all
+hist(out,[1:10]);
+setupPlot
+xlabel('Number of top-level components');
+ylabel('Frequency')
+makePDF('hdp_inference.eps');
---
+++ Code/Tasks/hdp_sample.m
@@ -0,0 +1,280 @@
+% Gibbs sampling for a hierarchical DP
+function o = hdp_sample(x)
+close all;
+% Data must be stored in x{}
+
+NITS = 300;
+
+base_mean = 0;
+base_prec = 1e-2;
+
+obs_prec = 1;
+
+
+alpha = 1;
+gamma = 1;
+
+J = length(x);
+
+resampletables = 1; % Set this to 1 to resample table identies 
+
+
+% initialise everything in one cluster
+top_N = 0;
+top_S = 0;
+N_top = 1;
+N_tables_per_top = J;
+allTop = {[]};
+for j = 1:J
+    N(j) = length(x{j});
+    tables_to_top{j} = [1];
+    Z{j} = repmat(1,N(j),1);
+    top_N = top_N + N(j);
+    top_S = top_S + sum(x{j});
+    N_tables(j) = 1;
+    table_sums{j} = N(j);
+end
+
+allN_top = [1];
+
+
+for it = 1:NITS
+    jorder = randperm(J);
+    for j = 1:J
+        thisj = jorder(j);
+        norder = randperm(N(thisj));
+        for n = 1:N(thisj)
+            thisn = norder(n);
+            
+            % Remove thisn from the model
+            thistable = Z{thisj}(thisn);
+            Z{thisj}(thisn) = -1;
+            thistop = tables_to_top{thisj}(thistable);
+            table_sums{thisj}(thistable) = table_sums{thisj}(thistable) - 1;
+            top_N(thistop) = top_N(thistop) - 1;
+            top_S(thistop) = top_S(thistop) - x{thisj}(thisn);
+            
+            if table_sums{thisj}(thistable) == 0
+                % Delete the table
+                table_sums{thisj}(thistable) = [];
+                tables_to_top{thisj}(thistable) = [];
+                N_tables(thisj) = N_tables(thisj) - 1;
+                N_tables_per_top(thistop) = N_tables_per_top(thistop) - 1;
+                pos = find(Z{thisj}>thistable);
+                Z{thisj}(pos) = Z{thisj}(pos) - 1;
+            end
+            
+            if N_tables_per_top(thistop) == 0
+                % Delete this top level component
+                N_tables_per_top(thistop) = [];
+                if top_N(thistop) ~= 0 
+                    keyboard
+                end
+                top_N(thistop) = [];
+                top_S(thistop) = [];
+                N_top = N_top - 1;
+                for jp = 1:J
+                    pos = find(tables_to_top{jp}>thistop);
+                    tables_to_top{jp}(pos) = tables_to_top{jp}(pos) - 1;
+                end
+            end
+            
+            
+            TableLike = zeros(1,N_tables(thisj)+1);
+            for table = 1:N_tables(thisj)
+                TableLike(table) = clusterlike(x{thisj}(thisn),top_N(tables_to_top{thisj}(table)),...
+                    top_S(tables_to_top{thisj}(table)),base_mean,base_prec,obs_prec);
+            end
+            TablePrior = [table_sums{thisj} alpha]./(alpha + sum(table_sums{thisj}));
+            
+            
+            newTableLike = zeros(1,N_top+1);
+            for i = 1:N_top
+                newTableLike(i) = clusterlike(x{thisj}(thisn),top_N(i),top_S(i),base_mean,base_prec,obs_prec);
+            end
+            newTableLike(end) = clusterlike(x{thisj}(thisn),0,0,base_mean,base_prec,obs_prec);
+            newTablePrior = [N_tables_per_top gamma]./(gamma + sum(N_tables_per_top));
+            
+            TableLike(end) = log(sum(exp(newTableLike).*newTablePrior)); % The margilised bit
+            
+            TablePost = TableLike + log(TablePrior);
+            TablePost = exp(TablePost - max(TablePost));
+            TablePost = cumsum(TablePost./sum(TablePost));
+            
+            newTable = find(rand<=TablePost,1);
+            
+            if newTable < N_tables(thisj)
+                % This is a current table
+                Z{thisj}(thisn) = newTable;
+                table_sums{thisj}(newTable) = table_sums{thisj}(newTable) + 1;
+                newTop = tables_to_top{thisj}(newTable);
+                top_N(newTop) = top_N(newTop) + 1;
+                top_S(newTop) = top_S(newTop) + x{thisj}(thisn);
+            else
+                % This is a new table
+                % Choose the component
+                topPost = newTableLike + log(newTablePrior);
+                topPost = exp(topPost - max(topPost));
+                topPost = cumsum(topPost./sum(topPost));
+                newTop = find(rand<=topPost,1);
+                N_tables(thisj) = N_tables(thisj) + 1;
+                if newTop < N_top
+                    % New table with current component
+                    tables_to_top{thisj}(1,end+1) = newTop;
+                    Z{thisj}(thisn) = max(Z{thisj}) + 1;
+                    table_sums{thisj}(1,end+1) = 1;
+                    top_N(newTop) = top_N(newTop) + 1;
+                    top_S(newTop) = top_S(newTop) + x{thisj}(thisn);
+                    N_tables_per_top(newTop) = N_tables_per_top(newTop) + 1;
+                else
+                    % New table with new component
+                    Z{thisj}(thisn) = max(Z{thisj}) + 1;
+                    table_sums{thisj}(1,end+1) = 1;
+                    N_top = N_top + 1;
+                    top_N(1,N_top) = 1;
+                    top_S(1,N_top) = x{thisj}(thisn);
+                    tables_to_top{thisj}(1,end+1) = N_top;
+                    N_tables_per_top(N_top) = 1;
+                end
+                
+            end
+            
+        end
+        checkModel(x,top_N,top_S,Z,tables_to_top,N_tables_per_top,N_top,N_tables);
+    end
+    
+    if resampletables
+        for j = 1:J
+            for k = 1:N_tables(j)
+                % Remove this table from the top-level component
+                thisTop = tables_to_top{j}(k);
+                N_tables_per_top(thisTop) = N_tables_per_top(thisTop) - 1;
+                pos = find(Z{j}==k);
+                top_N(thisTop) = top_N(thisTop) - length(pos);
+                top_S(thisTop) = top_S(thisTop) - sum(x{j}(pos));
+                if N_tables_per_top(thisTop) == 0
+                    % Delete the top level component
+                    top_N(thisTop) = [];
+                    top_S(thisTop) = [];
+                    N_top = N_top - 1;
+                    N_tables_per_top(thisTop) = [];
+                    for jp = 1:J
+                        tpos = find(tables_to_top{jp} > thisTop);
+                        tables_to_top{jp}(tpos) = tables_to_top{jp}(tpos) - 1;
+                    end
+                end
+                Like = zeros(1,N_top+1);
+                for i = 1:N_top
+                    Like(i) = 0;
+                    tempN = top_N(i);
+                    tempS = top_S(i);
+                    for p = 1:length(pos)
+                        Like(i) = Like(i) + clusterlike(x{j}(pos(p)),tempN,tempS,base_mean,base_prec,obs_prec);
+                        tempN = tempN + 1;
+                        tempS = tempS + x{j}(pos(p));
+                    end
+                end
+                Like(end) = 0;
+                tempN = 0;
+                tempS = 0;
+                for p = 1:length(pos)
+                    Like(end) = Like(end) + clusterlike(x{j}(pos(p)),tempN,tempS,base_mean,base_prec,obs_prec);
+                    tempN = tempN + 1;
+                    tempS = tempS + x{j}(pos(p));
+                end
+                Prior = [N_tables_per_top gamma];
+                Post = Like + Prior;
+                Post = exp(Post - max(Post));
+                Post = Post./sum(Post);
+                newTop = find(rand<=cumsum(Post),1);
+                if newTop > N_top
+                    N_top = N_top + 1;
+                    tables_to_top{j}(k) = N_top;
+                    N_tables_per_top(1,N_top) = 1;
+                    top_N(1,N_top) = length(pos);
+                    top_S(1,N_top) = sum(x{j}(pos));
+                else
+                   tables_to_top{j}(k) = newTop;
+                   N_tables_per_top(newTop) = N_tables_per_top(newTop) + 1;
+                   top_N(newTop) = top_N(newTop) + length(pos);
+                   top_S(newTop) = top_S(newTop) + sum(x{j}(pos));
+                end
+            end
+        end
+    end
+    
+    
+    % Get the top component means
+    postprec = base_prec + top_N*obs_prec;
+    postmean = (1./postprec).*(obs_prec.*top_S + base_mean*base_prec);
+    
+    allN_top(it) = N_top;
+  
+    
+    figure(1);
+    hold on
+    for i = 1:length(postmean)
+        plot(it,postmean(i),'k.');
+    end
+    figure(2)
+    hist(allN_top,[1:10]);
+    drawnow
+    
+    
+end
+o = allN_top;
+
+function l = clusterlike(x,top_N,top_S,base_mean,base_prec,obs_prec)
+
+postprec = base_prec + top_N*obs_prec;
+postmean = (1/postprec)*(obs_prec*top_S + base_mean*base_prec);
+predprec = 1/(1/postprec + 1/obs_prec);
+l = lognormpdf(x,postmean,predprec);
+
+function l = lognormpdf(x,m,p)
+
+l = -0.5*sqrt(2*pi) + 0.5*log(p) - 0.5*p*(x-m).^2;
+
+function o = checkModel(x,top_N,top_S,Z,tables_to_top,N_tables_per_top,N_top,N_tables)
+% This checks my bookkeeping is correct!
+J = length(x);
+
+if N_top ~= length(top_N)
+    keyboard
+end
+
+if N_top ~= length(top_S)
+    keyboard
+end
+
+new_t_t_t = zeros(1,N_top);
+for i = 1:N_top
+    for j = 1:J
+        new_t_t_t(i) = new_t_t_t(i) + sum(tables_to_top{j}==i);
+    end
+end
+
+if sum(new_t_t_t ~= N_tables_per_top) > 0
+    keyboard
+end
+
+new_top_N = zeros(1,N_top);
+new_top_S = zeros(1,N_top);
+for j = 1:J
+    for n = 1:length(x{j})
+        thistable = Z{j}(n);
+        thistop = tables_to_top{j}(thistable);
+        new_top_N(thistop) = new_top_N(thistop) + 1;
+        new_top_S(thistop) = new_top_S(thistop) + x{j}(n);
+    end
+end
+
+if sum(new_top_N ~= top_N) > 0
+    keyboard
+end
+
+if sum(abs(new_top_S-top_S)>1e-9) > 0
+    keyboard
+end
+
+
